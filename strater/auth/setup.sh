d491547f4957c91e98ed86c051dd140a629baeeb
Ieenah <66967074+Ieenah@users.noreply.github.com>
2021-09-02 20:35:10 +0300

    Delete setup.sh

--- strater/auth/setup.sh
+++
@@ -1,5 +0,0 @@
-
-export DATABASE_URL = 'postgresql://vrdfjsbeljadfv:3153e2f96cf0f1de8de336dec28c081faa9ea3e60e99ca56bc9efac11935917b@ec2-34-200-94-86.compute-1.amazonaws.com:5432/ddmjjqvbci198b'
-export AUTH0_DOMAIN='ileenah.us.auth0.com'
-export API_AUDIENCE='dev1'
-export ALGORITHMS=['RS256']
